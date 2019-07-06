843dd4d623208a2fb89fd8927d45a38d727bf7fa
TommyWorker <mn.toshi@gmail.com>
2019-07-07 00:38:01 +0900

    あ

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -1,15 +1,19 @@
 class RoomChannel < ApplicationCable::Channel
   def subscribed
-    # stream_from "some_channel"
+    stream_from "room_channel"
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
   end
 
-  def speakheroku
-  end
+  def speak(data)
 
-  def logs
+    @message = Message.new(talk_id:data['talk_id'], user_id:data['user_id'], content:data['message'])
+    if @message.save
+      #binding.pry
+      ActionCable.server.broadcast 'room_channel', data['message'] + ':::' + data['image'] + ':::' + data['name'] + ':::' + data['user_id']
+    end
+
   end
 end
